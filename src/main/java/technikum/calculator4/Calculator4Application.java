77bb68f613bf91193532df9c9a53496413f85e68
JuliaJakoubek <lilfae85@gmail.com>
2022-06-11 11:39:07 +0200

    Initial commit

---
+++ src/main/java/technikum/calculator4/Calculator4Application.java
@@ -0,0 +1,13 @@
+package technikum.calculator4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Calculator4Application.class, args);
+    }
+
+}
---
+++ src/test/java/technikum/calculator4/Calculator4ApplicationTests.java
@@ -0,0 +1,13 @@
+package technikum.calculator4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
